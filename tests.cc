7d669b587d328c76303bb9fbf4731082c6a04607
Robert Muth <robert@muth.org>
2025-02-21 20:17:23 -0500

    more work on tests

--- tests.cc
+++ tests.cc
@@ -32,6 +32,13 @@ void SplitAndGroup(std::vector<Token>* out, std::string_view s,
   out->push_back(End());
 }
 
+std::vector<Token> MakeTokensFromString(
+    std::string_view s, BreakType break_type = BreakType::INCONSISTENT) {
+  std::vector<Token> out;
+  SplitAndGroup(&out, s, break_type);
+  return out;
+}
+
 int Test() {
   std::vector<Token> example0 = {Beg(BreakType::FORCE_LINE_BREAK, 0),
                                  Str("procedure test(x, y: Integer);"),
@@ -70,29 +77,51 @@ int Test() {
                                End()};
 
   std::vector<Token> example3b = {Beg(BreakType::INCONSISTENT, 4),
-                     Str("XXXXXXXXXX"), Brk(), Str("+"), Brk(),
-                     Str("YYYYYYYYYY"), Brk(), Str("+"), Brk(),
-                     Str("ZZZZZZZZZZ"), End()
-};
+                                  Str("XXXXXXXXXX"),
+                                  Brk(),
+                                  Str("+"),
+                                  Brk(),
+                                  Str("YYYYYYYYYY"),
+                                  Brk(),
+                                  Str("+"),
+                                  Brk(),
+                                  Str("ZZZZZZZZZZ"),
+                                  End()};
 
-std::vector<Token> example6 = {{Beg(BreakType::INCONSISTENT, 6)}};
-SplitAndGroup(&example6, "cases 1 : XXXXX");
-example6.push_back(LineBreak());
-SplitAndGroup(&example6, "2 : YYYYY");
-example6.push_back(LineBreak());
-SplitAndGroup(&example6, "3 : ZZZZZ");
-example6.push_back(End());
+  std::vector<Token> example4 = {Beg(BreakType::INCONSISTENT, 2),
+                                 Str("begin"),
+                                 Brk(),
+                                 Str("x"),
+                                 Brk(),
+                                 Str(":="),
+                                 Brk(),
+                                 Str("40"),
+                                 Brk(),
+                                 Str("+"),
+                                 Brk(),
+                                 Str("2"),
+                                 Brk(),
+                                 Str("end"),
+                                 End()};
 
-struct {
-  size_t line_width;
-  std::vector<Token> tokens;
-  std::string expected;
-} test_suite[] = {
-    {75, example6, R"(cases 1 : XXXXX
+  std::vector<Token> example6 = {{Beg(BreakType::INCONSISTENT, 6)}};
+  SplitAndGroup(&example6, "cases 1 : XXXXX");
+  example6.push_back(LineBreak());
+  SplitAndGroup(&example6, "2 : YYYYY");
+  example6.push_back(LineBreak());
+  SplitAndGroup(&example6, "3 : ZZZZZ");
+  example6.push_back(End());
+
+  struct {
+    size_t line_width;
+    std::vector<Token> tokens;
+    std::string expected;
+  } test_suite[] = {
+      {75, example6, R"(cases 1 : XXXXX
       2 : YYYYY
       3 : ZZZZZ)"},
-    //
-    {75, example0, R"(procedure test(x, y: Integer);
+      //
+      {75, example0, R"(procedure test(x, y: Integer);
 begin
   x:=1;
   y:=200;
@@ -102,32 +131,41 @@ begin
   end;
   y:=x;
 end;)"},
-    //
-    {75,
-     {Beg(BreakType::INCONSISTENT, 2), Str("XXXXXXX"), Brk(), Str("YYYYYYY"),
-      End()},
-     "XXXXXXX YYYYYYY"},
-    //
-    {75, example3a, "XXXXXXXXXX + YYYYYYYYYY + ZZZZZZZZZZ"},
-    {25, example3a, "XXXXXXXXXX + YYYYYYYYYY +\n  ZZZZZZZZZZ"},
-    {20, example3a, "XXXXXXXXXX +\n  YYYYYYYYYY +\n  ZZZZZZZZZZ"},
-    {20, example3b, "XXXXXXXXXX +\n    YYYYYYYYYY +\n    ZZZZZZZZZZ"},
-};
+      //
+      {75,
+       {Beg(BreakType::INCONSISTENT, 2), Str("XXXXXXX"), Brk(), Str("YYYYYYY"),
+        End()},
+       "XXXXXXX YYYYYYY"},
+      //
+      {75, example3a, "XXXXXXXXXX + YYYYYYYYYY + ZZZZZZZZZZ"},
+      {25, example3a, "XXXXXXXXXX + YYYYYYYYYY +\n  ZZZZZZZZZZ"},
+      {20, example3a, "XXXXXXXXXX +\n  YYYYYYYYYY +\n  ZZZZZZZZZZ"},
+      {20, example3b, "XXXXXXXXXX +\n    YYYYYYYYYY +\n    ZZZZZZZZZZ"},
+      //
+      {75, example4, "begin x := 40 + 2 end"},
+      //
+      {20, MakeTokensFromString("locals a, b, c, d, e, f, g, h;"),
+       "locals a, b, c, d,\n  e, f, g, h;"},
+      {20,
+       MakeTokensFromString("locals a, b, c, d, e, f, g, h;",
+                            BreakType::CONSISTENT),
+       "locals\n  a,\n  b,\n  c,\n  d,\n  e,\n  f,\n  g,\n  h;"},
+  };
 
-for (const auto& test : test_suite) {
-  std::string result = PrettyPrint(test.tokens, test.line_width);
-  std::cout << "\n"
-            << "EXPECTED-START\n"
-            << test.expected << "\nEXPECTED-END\n";
-  if (result != test.expected) {
+  for (const auto& test : test_suite) {
+    std::string result = PrettyPrint(test.tokens, test.line_width);
     std::cout << "\n"
-              << "FAILED_START\n"
-              << result << "\nFAILED_END\n";
+              << "EXPECTED-START\n"
+              << test.expected << "\nEXPECTED-END\n";
+    if (result != test.expected) {
+      std::cout << "\n"
+                << "FAILED_START\n"
+                << result << "\nFAILED_END\n";
 
-    return 1;
+      return 1;
+    }
   }
-}
-return 0;
+  return 0;
 }
 
 int main() { return Test(); }
